14cadd59eea05d3e6ec65bae81fe156f4ffa008c
Arthur Leonard Andersen <leoc.git@gmail.com>
2013-12-14 13:27:53 +0100

    Add rack-test for testing sinatra

--- ledger-rest.gemspec
+++ ledger-rest.gemspec
@@ -24,6 +24,7 @@ Gem::Specification.new do |gem|
   gem.add_development_dependency 'rake'
   gem.add_development_dependency 'pry'
   gem.add_development_dependency 'rspec'
+  gem.add_development_dependency 'rack-test'
   gem.add_development_dependency 'rb-inotify'
   gem.add_development_dependency 'guard-rspec'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,14 @@
 require 'ledger-rest'
+require 'rack/test'
+
+module RSpecMixin
+  include Rack::Test::Methods
+  def app; LedgerRest::App; end
+end
 
 RSpec.configure do |config|
+  config.include RSpecMixin
+
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
