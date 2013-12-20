796d0ae679b92ae626d0a372fcc09b0123b7a651
Arthur Leonard Andersen <leoc.git@gmail.com>
2013-12-20 20:26:33 +0100

    Implement new budget format

--- lib/ledger-rest/ledger/budget.rb
+++ lib/ledger-rest/ledger/budget.rb
@@ -3,15 +3,19 @@ module LedgerRest
   class Ledger
     class Budget
       FORMAT = [
-                '{',
-                '        "name": %(quoted(account)),',
-                '        "amount": %(quoted(get_at(T, 0))),',
-                '        "budget": %(quoted(-get_at(T, 1)))',
-                '      },%/',
-                '    ],',
-                '    "total_amount": %(quoted(get_at(T, 0))),',
-                '    "total_budget": %(quoted(-get_at(T, 1)))'
-               ].join('\\n')
+                '%(scrub(get_at(display_total, 0)))\n',
+                '%(-scrub(get_at(display_total, 1)))\n',
+                '%(scrub(get_at(display_total, 1) + get_at(display_total, 0)))\n',
+                '%(get_at(display_total, 1) ? (100% * scrub(get_at(display_total, 0))) / -scrub(get_at(display_total, 1)) : 0)\n',
+                '%(account)\n',
+                '---\n',
+                '%/---\n',
+                '%$1\n',
+                '%$2\n',
+                '%$3\n',
+                '%$4\n',
+                '%/'
+               ].join
 
       class << self
         def get(query = nil, params = {})
@@ -21,9 +25,26 @@ module LedgerRest
         def json(query = nil, params = {})
           params = { '--format' => FORMAT }.merge(params)
           result = Ledger.exec("budget #{query}", params)
-          result.gsub!(/\},\n *?\]/, "}\n  ]")
-
-          "{\n  \"budget\": {\n    \"accounts\": [\n      #{result}\n  }\n}"
+          budget = {}
+          accounts, total = result.split("---\n---\n")
+          budget['accounts'] = accounts.split("---\n").map do |str|
+            val = str.split("\n")
+            {
+              'total'      => val[0].empty? ? '0' : val[0],
+              'budget'     => val[1].empty? ? '0' : val[1],
+              'difference' => val[2].empty? ? '0' : val[2],
+              'percentage' => val[3].empty? ? '0' : val[3],
+              'account'    => val[4].empty? ? '0' : val[4]
+            }
+          end
+          if total
+            val = total.split("\n")
+            budget['total']      = val[0].empty? ? '0' : val[0]
+            budget['budget']     = val[1].empty? ? '0' : val[1]
+            budget['difference'] = val[2].empty? ? '0' : val[2]
+            budget['percentage'] = val[3].empty? ? '0' : val[3]
+          end
+          budget.to_json
         end
       end
     end
---
+++ spec/ledger-rest/api/budget_spec.rb
@@ -0,0 +1,110 @@
+require 'spec_helper'
+
+describe '/budget' do
+  context 'with query' do
+    let(:valid_json) do
+      {
+        'accounts' =>
+        [
+         {
+           'total'      => '3177.80EUR',
+           'budget'     => '-6600.00EUR',
+           'difference' => '9777.80EUR',
+           'percentage' => '-48%',
+           'account'    => 'Assets'
+         }, {
+           'total'      => '53.20EUR',
+           'budget'     => '6600.00EUR',
+           'difference' => '-6546.80EUR',
+           'percentage' => '1%',
+           'account'    => 'Expenses'
+         }, {
+           'total'      => '0',
+           'budget'     => '5400.00EUR',
+           'difference' => '-5400.00EUR',
+           'percentage' => '0%',
+           'account'    => 'Expenses:Flat'
+         }, {
+           'total'      => '53.20EUR',
+           'budget'     => '1200.00EUR',
+           'difference' => '-1146.80EUR',
+           'percentage' => '4%',
+           'account'    => 'Expenses:Restaurants'
+         }
+        ],
+        'total'      => '3231.00EUR',
+        'budget'     => '0',
+        'difference' => '3231.00EUR',
+        'percentage' => '0'
+      }
+    end
+
+    it 'responds with the right budget' do
+      get '/budget'
+
+      JSON.parse(last_response.body).should deep_eq valid_json
+    end
+  end
+
+  context 'querying single account' do
+    let(:valid_json) do
+      {
+        'accounts' =>
+        [
+         {
+           'total'      => '53.20EUR',
+           'budget'     => '100.00EUR',
+           'difference' => '-46.80EUR',
+           'percentage' => '53%',
+           'account'    => 'Expenses:Restaurants'
+         }
+        ]
+      }
+    end
+
+    it 'responds with the right budget' do
+      get '/budget', query: 'Restaurants'
+
+      JSON.parse(last_response.body).should deep_eq valid_json
+    end
+  end
+
+  context 'querying single account' do
+    let(:valid_json) do
+      {
+        'accounts' =>
+        [
+         {
+           'total'      => '53.20EUR',
+           'budget'     => '550.00EUR',
+           'difference' => '-496.80EUR',
+           'percentage' => '10%',
+           'account'    => 'Expenses'
+         }, {
+           'total'      => '0',
+           'budget'     => '450.00EUR',
+           'difference' => '-450.00EUR',
+           'percentage' => '0%',
+           'account'    => 'Expenses:Flat'
+         }, {
+           'total'      => '53.20EUR',
+           'budget'     => '100.00EUR',
+           'difference' => '-46.80EUR',
+           'percentage' => '53%',
+           'account'    => 'Expenses:Restaurants'
+         }
+        ],
+        'total'      => '53.20EUR',
+        'budget'     => '550.00EUR',
+        'difference' => '-496.80EUR',
+        'percentage' => '10%',
+      }
+    end
+
+    it 'responds with the right budget' do
+      get '/budget', query: 'Expenses'
+
+      JSON.parse(last_response.body).should deep_eq valid_json
+    end
+  end
+end
