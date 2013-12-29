c80567d3c7db5775ff7c6d859f590196a991a8cc
Arthur Leonard Andersen <leoc.git@gmail.com>
2013-12-29 14:44:18 +0100

    Add multi commodity totals to balance report

--- lib/ledger-rest/ledger/balance.rb
+++ lib/ledger-rest/ledger/balance.rb
@@ -4,11 +4,11 @@ module LedgerRest
     class Balance
       FORMAT = [
                 '{',
-                '"total":%(quoted(display_total)),',
+                '"totals":[[%(quoted(scrub(display_total)))]],',
                 '"name":%(quoted(partial_account)),',
                 '"depth":%(depth), "fullname": %(quoted(account))',
-                '},%/',
-                '{"total":%(quoted(display_total))}'
+                '},%/],',
+                '"totals":[[%(quoted(scrub(display_total)))]]'
                ].join
 
       class << self
@@ -25,23 +25,17 @@ module LedgerRest
 
         def json(query = nil, params = {})
           params = { '--format' => FORMAT }.merge(params)
+          puts "bal #{query}"
           result = Ledger.exec("bal #{query}", params)
 
-          total = nil
-          if result.end_with?(',')
-            result = result[0..-2]
-          else
-            match_total = result.match(/,{"total":("[0-9\.A-Za-z ]+")}\z/)
-            if match_total
-              total = match_total[1]
-              result = result[0, match_total.offset(0)[0]]
-            end
+          parser = Ledger::Parser.new
+          result.gsub! /\[\["(?<amounts>[^"]+)"\]\]/m do |a, b|
+            $~[:amounts].split("\n").map do |str|
+              amount, commodity = parser.parse_amount_parts(str)
+              { amount: amount, commodity: commodity }
+            end.to_json
           end
-
-          json_str = '{'
-          json_str << "\"accounts\":[#{result}]"
-          json_str << ",\"total\":#{total}" if total
-          json_str << '}'
+          "{\"accounts\":[#{result.gsub(',]', ']')}}"
         end
 
         def expand_accounts(accounts)
