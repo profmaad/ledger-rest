964d27755d70fee44564fb16abb4f095f8e37fcd
Arthur Leonard Andersen <leoc.git@gmail.com>
2014-01-01 20:17:20 +0100

    Fix missing root accounts when using --flat flag

--- spec/ledger-rest/api/balance_spec.rb
+++ spec/ledger-rest/api/balance_spec.rb
@@ -138,93 +138,48 @@ describe '/balance' do
         'accounts' =>
         [
          {
-           'totals' =>
-           [
-            {
-              'amount'    => 0.25,
-              'commodity' => 'BTC'
-            }
-           ],
+           'totals' => [{ 'amount' => 0.25, 'commodity' => 'BTC' }, { 'amount' => 3022.97, 'commodity' => 'EUR' }],
+           'name' => 'Assets',
+           'depth' => 1,
+           'fullname' => 'Assets'
+         }, {
+           'totals' => [{ 'amount'    => 0.25, 'commodity' => 'BTC' }],
            'name'     => 'Bitcoin',
            'depth'    => 2,
            'fullname' => 'Assets:Bitcoin'
          }, {
-           'totals' =>
-           [
-            {
-              'amount'    => 5.7,
-              'commodity' => 'EUR'
-            }
-           ],
+           'totals' => [{ 'amount'    => 5.7, 'commodity' => 'EUR' }],
            'name'     => 'Cash',
            'depth'    => 2,
            'fullname' => 'Assets:Cash'
          }, {
-           'totals'  =>
-           [
-            {
-              'amount'    => 2995.17,
-              'commodity' => 'EUR'
-            }
-           ],
+           'totals'  => [{ 'amount'    => 2995.17, 'commodity' => 'EUR' }],
            'name'     => 'Giro',
            'depth'    => 2,
            'fullname' => 'Assets:Giro'
          }, {
-           'totals'  =>
-           [
-            {
-              'amount'    => 22.1,
-              'commodity' => 'EUR'
-            }
-           ],
+           'totals'  => [{ 'amount'    => 22.1, 'commodity' => 'EUR' }],
            'name'     => 'Reimbursements',
            'depth'    => 2,
            'fullname' => 'Assets:Reimbursements'
          }, {
-           'totals'  =>
-           [
-            {
-              'amount'    => 22.1,
-              'commodity' => 'EUR'
-            }
-           ],
+           'totals'  => [{ 'amount'    => 22.1, 'commodity' => 'EUR' }],
            'name'     => 'Hans Maulwurf',
            'depth'    => 3,
            'fullname' => 'Assets:Reimbursements:Hans Maulwurf'
          }, {
-           'totals'  =>
-           [
-            {
-              'amount'    => -12.0,
-              'commodity' => 'EUR'
-            }
-           ],
+           'totals'  => [{ 'amount'    => -12.0, 'commodity' => 'EUR' }],
            'name'     => 'Liabilities',
            'depth'    => 1,
            'fullname' => 'Liabilities'
          }, {
-           'totals'  =>
-           [
-            {
-              'amount'    => -12.0,
-              'commodity' => 'EUR'
-            }
-           ],
+           'totals'  => [{ 'amount'    => -12.0, 'commodity' => 'EUR' }],
            'name'     => 'Max Mustermann',
            'depth'    => 2,
-           'fullname' => 'Liabilities:Max Mustermann' }
-        ],
-        'totals' =>
-        [
-         {
-           'amount'    => 0.25,
-           'commodity' => 'BTC'
-         }, {
-           'amount'    => 3010.97,
-           'commodity' => 'EUR'
+           'fullname' => 'Liabilities:Max Mustermann'
          }
-        ]
+        ],
+        'totals' => [{ 'amount'    => 0.25, 'commodity' => 'BTC' }, { 'amount' => 3010.97, 'commodity' => 'EUR' }]
       }
     end
 
