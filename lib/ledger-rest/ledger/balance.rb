1a486367ceeda36e5620a9da5baeed8d1c53575a
Arthur Leonard Andersen <leoc.git@gmail.com>
2014-01-01 17:30:39 +0100

    Remove debug output

--- lib/ledger-rest/ledger/balance.rb
+++ lib/ledger-rest/ledger/balance.rb
@@ -25,7 +25,6 @@ module LedgerRest
 
         def json(query = nil, params = {})
           params = { '--format' => FORMAT }.merge(params)
-          puts "bal #{query}"
           result = Ledger.exec("bal #{query}", params)
 
           parser = Ledger::Parser.new
